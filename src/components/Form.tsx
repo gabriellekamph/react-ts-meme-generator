2bee6a0b81811d44e6874cab1c9287f5a89d06ed
Gabrielle Kamph <gabriellekamph@Gabrielles-Air.lan>
2021-10-08 21:43:55 +0200

    Adding buttons to form component

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 
 // Interface for Form component
 
@@ -13,3 +13,63 @@ interface FormInterface {
     handleMemeReset: () => void;
 }
 
+// Form component 
+
+const Form = (props: FormInterface) => {
+    return (
+        <div className="form">
+            <div className="form-inputs">
+                { /* Input field for top text */ }
+                <input
+                    name="text-top"
+                    type="text"
+                    placeholder="Top text"
+                    value={props.textTop}
+                    onChange={props.handleInputChange}
+                />
+
+                { /* Input field for bottom text */ }
+                <input
+                    name="text-bottom"
+                    type="text"
+                    placeholder="Bottom text"
+                    value={props.textBottom}
+                    onChange={props.handleInputChange}
+                />
+            </div>
+
+            <div className="form-buttons">
+                { /* Button to load random image from API on click */ }
+                <button className="btn btn-primary" type="button" onClick={props.handleImageChange}>
+                    Change image
+                </button>
+
+                { /* Load image */ }
+                <label className="btn btn-primary" htmlFor="fileInput">
+                    Load image 
+                    <input 
+                        id="fileInput" 
+                        name="fileInput" 
+                        type="file" 
+                        accept=".jpg, .jpeg, .png" 
+                        onChange={props.handleImageInputChange} 
+                        hidden 
+                    />                
+                </label>
+
+                { /* Button to generate png of the meme on click */}
+                <button className="btn btn-primary" type="button" onClick={props.handleMemeGeneration}>
+                    Generate meme
+                </button>
+
+                { /* Button to remove meme on click */}
+                {props.isMemeGenerated && 
+                <button className="btn btn-danger" type="button onClick={props.handleMemeReset">
+                    Reset 
+                </button>}
+            </div>
+        </div>
+    )
+}
+
+export default Form;
