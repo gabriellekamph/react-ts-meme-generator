43c00d7edc8af7566c72fde9df6b0bb79afb21d3
Gabrielle Kamph <gabriellekamph@Gabrielles-Air.lan>
2021-10-08 21:49:46 +0200

    Creating content component with image preview and h1 + h2 for displaying text

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -9,3 +9,19 @@ interface ContentInterface {
     textTop: string; 
 }
 
+const Content = (props: ContentInterface) => {
+    return (
+        <div className="content" ref={props.contentContainerRef}>
+            { /* Preview image */ }
+            <img src={props.activeImage} alt="Meme" />
+
+            { /* Top text */ }
+            <h1>{props.textTop}</h1>
+
+            { /* Bottom text */ }
+            <h2>{props.textBottom}</h2>
+        </div>
+    )
+}
+
+export default Content;
