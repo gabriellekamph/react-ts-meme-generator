b4709f2cd191fa035c75634c4fd785f4abed3f62
Gabrielle Kamph <gabriellekamph@Gabrielles-Air.lan>
2021-10-08 21:54:40 +0200

    Installing Typescript

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
       <h1> Meme Generator</h1>
+      <Form />
     </div>
   );
 }
