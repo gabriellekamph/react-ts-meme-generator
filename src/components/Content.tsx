cb4bf408868ccfb9c633ed7b4042e90d860725cf
Gabrielle Kamph <gabriellekamph@Gabrielles-Air.lan>
2021-10-08 21:46:40 +0200

    Creating content interface for typescript

---
+++ src/components/Content.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+// Interface for Content component 
+
+interface ContentInterface {
+    activeImage: string;
+    contentContainerRef: React.RefObject<any>;
+    textBottom: string;
+    textTop: string; 
+}
+
