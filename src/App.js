cf348409d13a45cad70212a82a3cb013bce10917
Gabrielle Kamph <gabriellekamph@Gabrielles-Air.lan>
2021-10-09 08:07:09 +0200

    Creating refs and hooks

--- src/App.js
+++ src/App.js
@@ -5,7 +5,20 @@ import domtoimage from 'dom-to-image-more';
 import Form from './components/Form';
 
 
-// CONTINUE WITH REFS AND STATES
+// Creating refs
+
+let contentContainerRef = React.useRef<HTMLElement | null>(null)
+let resultContainerRef = React.useRef<HTMLElement | null>(null)
+
+// Creating hooks for useState
+
+const [images, setImages] = React.useState([])
+const [activeImage, setActiveImage] = React.useState('')
+const [textTop, setTextTop] = React.useState('')
+const [textBottom, setTextBottom] = React.useState('')
+const [isMemeGenerated, setIsMemeGenerated] = React.useState(false)
+
+
 
 function App() {
   return (
